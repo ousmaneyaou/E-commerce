5e906d0dd89efaa8d12ea073e2400de09c9ecf85
ousmane <ousmaneyaou00@gmail.com>
2024-08-09 00:22:23 +0000

    creation du maquette login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import '../Pages/CSS/Login.css'
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className='login'>
+      <div className="loginsignup-container">
+        <h1>S'inscrire</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Votre nom' />
+          <input type="email" placeholder='Adress email' />
+          <input type="password"placeholder='Mot de pass' />
+        </div>
+        <button>Continuer</button>
+        <div className="loginsignup-login">
+          vous avez déjà un compte ? <samp>Se connecter ici</samp>
+          <div className="loginsignup-agree">
+            <input type="checkbox" name='' id='' />
+            <p>En continuant, j'accepte les conditions d'utilisation et la politique de confidentialité.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
